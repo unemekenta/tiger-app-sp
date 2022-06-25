6b65d05b7c95b6772b134661408b9e3c911273a3
unemekenta <unemekenta@gmail.com>
2022-06-25 14:30:00 +0900

    fix.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { NativeBaseProvider} from 'native-base';
+import React from 'react';
+import { NativeBaseProvider } from 'native-base';
 import HomeStack from './components/organisms/HomeStack';
+import { Store } from './Store';
 
 export default function App() {
   return (
-    <>
+    <Store>
       <NativeBaseProvider>
-        <HomeStack/>
+        <HomeStack />
       </NativeBaseProvider>
-    </>
+    </Store>
   );
 }
